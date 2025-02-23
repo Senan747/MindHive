505bd50393bd73a4cdaa10604bff1057b74b872d
Senan747 <1senanmemmedov@gmail.com>
2025-02-24 01:36:53 +0400

    feat: add blog-card component to home page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,17 @@
 import Image from "next/image";
 import SearchForm from "@/components/SearchForm";
+import BlogCard from "@/components/BlogCard";
+
+interface BlogType {
+  title: string;
+  id: number;
+  view: number;
+  createdAt: string;
+  author: { id: number; name: string };
+  profile_image: string;
+  blog_image: string;
+  category: string;
+}
 
 export default async function Home({
   searchParams,
@@ -7,8 +19,60 @@ export default async function Home({
   searchParams: Promise<{ query?: string }>;
 }) {
   const query = (await searchParams).query;
+
+  const blogs = [
+    {
+      id: 1,
+      createdAt: new Date().toISOString(),
+      author: { id: 1, name: "Sanan" },
+      title: "The Impact of Technology on the Workplace",
+      view: 77,
+      profile_image:
+        "https://plus.unsplash.com/premium_photo-1689568126014-06fea9d5d341?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8cHJvZmlsZXxlbnwwfHwwfHx8MA%3D%3D",
+      blog_image:
+        "https://images.unsplash.com/photo-1550751827-4bd374c3f58b?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fHRlY2hub2xvZ3l8ZW58MHx8MHx8fDA%3D",
+      category: "Technology",
+    },
+    {
+      id: 2,
+      createdAt: new Date().toISOString(),
+      author: { id: 2, name: "Sanan" },
+      title: "React js",
+      view: 77,
+      profile_image:
+        "https://t4.ftcdn.net/jpg/03/64/21/11/360_F_364211147_1qgLVxv1Tcq0Ohz3FawUfrtONzz8nq3e.jpg",
+      blog_image:
+        "https://images.unsplash.com/photo-1504639725590-34d0984388bd?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8aW5mb3JtYXRpb24lMjB0ZWNobm9sb2d5fGVufDB8fDB8fHww",
+      category: "Technology",
+    },
+    {
+      id: 3,
+      createdAt: new Date().toISOString(),
+      author: { id: 2, name: "Sanan" },
+      title: "React js",
+      view: 77,
+      profile_image:
+        "https://t4.ftcdn.net/jpg/03/64/21/11/360_F_364211147_1qgLVxv1Tcq0Ohz3FawUfrtONzz8nq3e.jpg",
+      blog_image:
+        "https://images.unsplash.com/photo-1504639725590-34d0984388bd?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8aW5mb3JtYXRpb24lMjB0ZWNobm9sb2d5fGVufDB8fDB8fHww",
+      category: "Technology",
+    },
+    {
+      id: 4,
+      createdAt: new Date().toISOString(),
+      author: { id: 2, name: "Sanan" },
+      title: "React js",
+      view: 77,
+      profile_image:
+        "https://t4.ftcdn.net/jpg/03/64/21/11/360_F_364211147_1qgLVxv1Tcq0Ohz3FawUfrtONzz8nq3e.jpg",
+      blog_image:
+        "https://images.unsplash.com/photo-1504639725590-34d0984388bd?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8aW5mb3JtYXRpb24lMjB0ZWNobm9sb2d5fGVufDB8fDB8fHww",
+      category: "Technology",
+    },
+  ];
+
   return (
-    <div className="center flex-col py-14">
+    <div className="center flex-col py-14 gap-10">
       <section className="w-full center relative">
         <Image
           src={"/home.svg"}
@@ -24,7 +88,7 @@ export default async function Home({
           className="absolute top-10 right-20"
         />
       </section>
-      <section className="pt-10">
+      <section>
         <p className="font-merriweather font-semibold text-4xl text-center">
           Show the World What You're Building.
         </p>
@@ -38,6 +102,24 @@ export default async function Home({
           </span>
           <SearchForm query={query} />
         </div>
+      </section>
+      <section className="center flex-col">
+        <p className="text-4xl font-bold font-mono">
+          {query ? `Search results for ${query}` : "All Articles"}
+        </p>
+        <Image
+          src={"/curly-divider.webp"}
+          alt="curly-divider"
+          width={200}
+          height={10}
+        />
+        <ul className="grid grid-cols-3 gap-[20px] mt-4">
+          {blogs?.length > 0
+            ? blogs.map((blog: BlogType, index: number) => {
+                return <BlogCard blog={blog} key={blog?.id} />;
+              })
+            : "No article found"}
+        </ul>
       </section>
     </div>
   );
---
+++ components/BlogCard.tsx
@@ -0,0 +1,59 @@
+import React from "react";
+import Image from "next/image";
+import { formatDate } from "@/lib/utils";
+
+interface BlogType {
+  title: string;
+  id: number;
+  view: number;
+  createdAt: string;
+  author: { id: number; name: string };
+  profile_image: string;
+  blog_image: string;
+  category: string;
+}
+
+const BlogCard = ({ blog }: { blog: BlogType }) => {
+  return (
+    <li className="flex items-start p-[16px] flex-col  border border-[rgb(232,232,234)] rounded-2xl w-[392px] h-[500px]">
+      <div className="w-[360px] h-[240px]">
+        <img
+          src={blog?.blog_image}
+          alt=""
+          className="w-full h-full rounded-md"
+        />
+      </div>
+      <div className="rounded-lg text-[#4B6BFB] bg-[#F6F7FF] text-[14px] font-sans py-[4px] px-[10px] mt-[24px] mb-[16px]">
+        <span>{blog?.category}</span>
+      </div>
+      <div className="h-[80px]">
+        <span className="text-[24px] font-semibold font-sans h-[80px]">
+          {blog.title}
+        </span>
+      </div>
+      <div className="mt-3 flex gap-2">
+        <img
+          src={blog?.profile_image}
+          alt="profile-image"
+          className="w-7 h-7 rounded-full object-cover"
+        />
+        <span className="text-[#97989F] text-[16px] font-sans">
+          {blog?.author?.name}
+        </span>
+      </div>
+      <div className="pt-3 flex flex-row items-center justify-between w-full">
+        <span className="text-[#97989F] text-[16px] font-sans">
+          {formatDate(blog?.createdAt)}
+        </span>
+        <div className="flex flex-row gap-1">
+          <Image src="/eye-icon.png" alt="eye-icon" width={20} height={20} />
+          <span className="text-[#97989F] text-[16px] font-sans">
+            {blog?.view}
+          </span>
+        </div>
+      </div>
+    </li>
+  );
+};
+
+export default BlogCard;
